aa06ce5d5775eb0cc9cb0eabf3a88a9b9b68fad7
BohdanFSD-temp <luitko009@gmail.com>
2023-09-15 07:21:09 +0000

    added return book endpoint

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,10 +1,13 @@
 import datetime
 
-from rest_framework.permissions import IsAuthenticated
+from django.db import transaction
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework import viewsets
 
 from borrowing.models import Borrowing
 from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer, BorrowingRetrieveSerializer, BorrowingCreateSerializer
+from rest_framework.response import Response
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
@@ -15,7 +18,7 @@ class BorrowingViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == "list":
             return BorrowingListSerializer
-        if self.action == "retrieve":
+        if self.action in ("retrieve", "return_book"):
             return BorrowingRetrieveSerializer
         if self.action == 'create':
             return BorrowingCreateSerializer
@@ -37,3 +40,19 @@ class BorrowingViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user, borrow_date=datetime.date.today())
+
+    @action(methods=['get'], detail=True, permission_classes=[IsAdminUser],
+            url_path='return', url_name='return_book')
+    def return_book(self, request, pk=None):
+        """Endpoint for returning book, if book is already returned, return appropriate message"""
+        with transaction.atomic():
+            borrowing = self.get_object()
+            if not borrowing.actual_return:
+                borrowing.book.inventory += 1
+                borrowing.book.save()
+                actual_return = datetime.date.today()
+                serializer = self.get_serializer_class()(borrowing, data={'actual_return': actual_return}, partial=True)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                return Response(serializer.data, status=200)
+            return Response({'actual_return': 'book has already returned'}, status=200)
