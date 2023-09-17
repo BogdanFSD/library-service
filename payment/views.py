6f644ac7e058edd80ab494c4c6f4e002f4561e9a
BohdanFSD-temp <luitko009@gmail.com>
2023-09-17 17:24:31 +0000

    created succes view

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from payment.models import Payment
 from payment.serializers import PaymentSerializer
+
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
@@ -15,3 +18,14 @@ class PaymentViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(borrowing__user=self.request.user)
 
         return queryset
+
+    @action(methods=['get'], detail=False, permission_classes=[permissions.AllowAny],
+            url_path='success', url_name='success')
+    def payment_success(self, request):
+        """Success Payment view, mark payment as paid"""
+        session = request.query_params.get('session_id')
+        payment = Payment.objects.get(session_id=session)
+        serializer = self.get_serializer_class()(payment, data={'status': 'PAID'}, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=200)
